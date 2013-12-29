e9e0cda3931481d7361e03c688bb63894a8f3dc9
Christoffer Dall <christoffer.dall@linaro.org>
2013-12-29 00:34:38 -0800

    lib: Add debug macro to test_util
    Add a debug macro that resolves to printf only if you have DEBUG defined
    in your file.
    
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- lib/test_util.h
+++ lib/test_util.h
@@ -8,6 +8,14 @@
 #define pass(fmt...) printf("PASS: " fmt)
 #define fail(fmt...) printf("FAIL: " fmt)
 
+#ifdef DEBUG
+#define debug(fmt, ...) \
+	do { printf(fmt , ## __VA_ARGS__); } while (0)
+#else
+#define debug(fmt, ...) do {} while(0)
+#endif
+
+
 bool enough_args(int nargs, int needed);
 bool check_u32(u32 val, int base, char *expected);
 #endif
