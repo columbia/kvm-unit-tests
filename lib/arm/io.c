e8d994a266605f1bbf0bda0397b49c580c5a4cb1
Christoffer Dall <christoffer.dall@linaro.org>
2014-01-01 15:59:14 -0800

    arm: Fix and synchronize ARM puts implementation
    Use writeb to output bytes to the pl011 serial port instead of pointer
    dereference.
    
    Add a spinlock around the puts operations, except if the MMU is
    disabled, since the spin_lock() operation will never return in that case
    (strex fails to update memory).  Allowing a print without taking the
    spinlock when the MMU is disabled does not interfere with the validity
    of testing and allows early boot code (like the MMU enablement code
    itslef) to do early printf or debug prints, while synchronizing
    remaining SMP code.
    
    Signed-off-by: Christoffer Dall <christoffer.dall@linaro.org>

--- lib/arm/io.c
+++ lib/arm/io.c
@@ -1,14 +1,34 @@
 #include "libcflat.h"
 #include "libio.h"
 #include "iomaps.h"
+#include "arm/mmu.h"
+#include "arm/processor.h"
 #include "virtio-testdev.h"
 
 static volatile u8 *uart0_base;
 
-void puts(const char *s)
+static struct spinlock uart_lock = { 0 };
+
+static void __puts_early(const char *s)
 {
 	while (*s)
-		*uart0_base = *s++;
+		writeb(*s++, uart0_base);
+}
+
+static void __puts(const char *s)
+{
+	spin_lock(&uart_lock);
+	while (*s)
+		writeb(*s++, uart0_base);
+	spin_unlock(&uart_lock);
+}
+
+void puts(const char *s)
+{
+	if (!mmu_enabled())
+		__puts_early(s);
+	else
+		__puts(s);
 }
 
 void exit(int code)
